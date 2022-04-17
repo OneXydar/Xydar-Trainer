cdc81ef27d8b56c502075cd513dc274834b4fb81
OneXydar <103818274+OneXydar@users.noreply.github.com>
2022-04-17 16:22:10 +0200

    Update Xydar Trainer.user.js

--- Xydar Trainer.user.js
+++ Xydar Trainer.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name         Xydar Trainer
-// @version      0.1
+// @version      2.0v
 // @author       Xydar
+// @description  This cheat was developed by Xydar, made with <3
 // @match        https://kogama.com.br/page/webgl-frame/*
 // @match        https://www.kogama.com/page/webgl-frame/*
 // @match        https://friends.kogama.com/page/webgl-frame/*
